b0fb29ddf78f52408f3ee089ef8baef1441d31d1
e <jiajixu@qq.com>
2018-01-12 09:40:49 +0800

    iterator

---
+++ src/com/xujiaji/thingking/_11/ListIteration.java
@@ -0,0 +1,35 @@
+package com.xujiaji.thingking._11;
+
+import typeinfo.pets.Pet;
+import typeinfo.pets.Pets;
+
+import java.util.List;
+import java.util.ListIterator;
+
+/**
+ * Created by jiaji on 2018/1/12.
+ */
+public class ListIteration {
+    public static void main(String[] args) {
+        List<Pet> pets = Pets.arrayList(8);
+        ListIterator<Pet> it = pets.listIterator();
+        while (it.hasNext())
+        {
+            System.out.print(it.next() + ", " + it.nextIndex() + ", " + it.previousIndex() + "; ");
+        }
+        System.out.println();
+        while (it.hasPrevious())
+        {
+            System.out.print(it.previous().id() + " ");
+        }
+        System.out.println();
+        System.out.println(pets);
+        it = pets.listIterator(3);
+        while (it.hasNext())
+        {
+            it.next();
+            it.set(Pets.randomPet());
+        }
+        System.out.println(pets);
+    }
+}
---
+++ src/com/xujiaji/thingking/_11/SimpleIteration.java
@@ -0,0 +1,37 @@
+package com.xujiaji.thingking._11;
+
+import typeinfo.pets.Pet;
+import typeinfo.pets.Pets;
+
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Created by jiaji on 2018/1/12.
+ */
+public class SimpleIteration {
+    public static void main(String[] args) {
+        List<Pet> pets = Pets.arrayList(12);
+        Iterator<Pet> it = pets.iterator();
+        while (it.hasNext())
+        {
+            Pet p = it.next();
+            System.out.print(p.id() + ":" + p + " ");
+        }
+
+        System.out.println();
+
+        for (Pet p : pets)
+        {
+            System.out.print(p.id() + ":" + p + " ");
+        }
+
+        System.out.println();
+        it = pets.iterator();
+        for (int i = 0; i < 6; i++) {
+            it.next();
+            it.remove();
+        }
+        System.out.println(pets);
+    }
+}
