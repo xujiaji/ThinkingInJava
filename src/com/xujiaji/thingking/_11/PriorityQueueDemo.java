6b848760d96aca5ca3d069cd4c1eb6811c68f57f
xujiaji-linux <jiajixu@qq.com>
2018-01-19 21:06:20 +0800

    Queue

---
+++ src/com/xujiaji/thingking/_11/PriorityQueueDemo.java
@@ -0,0 +1,18 @@
+package com.xujiaji.thingking._11;
+
+import java.util.PriorityQueue;
+import java.util.Random;
+
+public class PriorityQueueDemo
+{
+    public static void main(String[] args)
+    {
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+        Random ran = new Random(47);
+        for (int i = 0; i < 10; i++)
+        {
+            priorityQueue.offer(ran.nextInt(i + 10));
+        }
+        QueueDemo.printQ(priorityQueue);
+    }
+}
