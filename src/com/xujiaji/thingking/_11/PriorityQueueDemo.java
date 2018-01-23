9c4a968090b55010fe7069e45b898941b0357cc0
xujiaji-linux <jiajixu@qq.com>
2018-01-23 17:01:58 +0800

    PriorityQueue

--- src/com/xujiaji/thingking/_11/PriorityQueueDemo.java
+++ src/com/xujiaji/thingking/_11/PriorityQueueDemo.java
@@ -1,7 +1,6 @@
 package com.xujiaji.thingking._11;
 
-import java.util.PriorityQueue;
-import java.util.Random;
+import java.util.*;
 
 public class PriorityQueueDemo
 {
@@ -14,5 +13,28 @@ public class PriorityQueueDemo
             priorityQueue.offer(ran.nextInt(i + 10));
         }
         QueueDemo.printQ(priorityQueue);
+        List<Integer> ints = Arrays.asList(25, 22, 20,
+                18, 14, 9, 3, 1, 1, 2, 3, 9, 14, 18, 21, 23, 25);
+        priorityQueue = new PriorityQueue<>(ints);
+        QueueDemo.printQ(priorityQueue);
+        priorityQueue = new PriorityQueue<>(ints.size(), Collections.reverseOrder());
+        priorityQueue.addAll(ints);
+        QueueDemo.printQ(priorityQueue);
+
+        String fact = "EDUCATION SHOULD ESCHEW OBFUSCATION";
+        List<String> strings = Arrays.asList(fact.split(""));
+        PriorityQueue<String> stringPQ = new PriorityQueue<>(strings);
+        QueueDemo.printQ(stringPQ);
+        stringPQ = new PriorityQueue<>(ints.size(), Collections.reverseOrder());
+        stringPQ.addAll(strings);
+        QueueDemo.printQ(stringPQ);
+
+        Set<Character> charSet = new HashSet<>();
+        for (char c : fact.toCharArray())
+        {
+            charSet.add(c);
+        }
+        PriorityQueue<Character> characterPQ = new PriorityQueue<>(charSet);
+        QueueDemo.printQ(characterPQ);
     }
 }
